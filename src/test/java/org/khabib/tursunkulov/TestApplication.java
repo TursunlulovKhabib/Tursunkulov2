fe54a901a05f5deec699159b0620905ab19d7b51
abikknail <you@example.com>
2025-02-09 23:41:42 +0300

    init-commit 2

---
+++ src/test/java/org/khabib/tursunkulov/TestApplication.java
@@ -0,0 +1,11 @@
+package org.khabib.tursunkulov;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
